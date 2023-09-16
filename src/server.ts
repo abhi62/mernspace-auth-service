e5dc7ecf7699dc9b2ea5eb2f39d0a706dc8be0a1
abhi62 <abhi.leon.93@gmail.com>
2023-09-17 00:11:10 +0530

    add winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
     const PORT = Config.PORT;
 
     try {
+        // throw new Error('Something Went Wrong');
+        logger.debug('debug message', {});
         app.listen(PORT, () => {
-            console.log(`server is running on port ${PORT}`);
+            logger.info(`server is running on port ${PORT}`);
         });
     } catch (error) {
-        console.log('Error starting server: ', error);
-        process.exit(1);
+        if (error instanceof Error) {
+            logger.error('Error starting server: ', error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
